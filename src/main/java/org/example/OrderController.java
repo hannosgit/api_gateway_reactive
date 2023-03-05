7bdfb086a41b4f6caadd1a11dae768dfe7ee8d76
Hannes Oswald <hannes.oswald97@gmail.com>
2023-03-05 16:54:08 +0100

    fix auth

--- src/main/java/org/example/OrderController.java
+++ src/main/java/org/example/OrderController.java
@@ -3,10 +3,14 @@ package org.example;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
+    private static final Base64.Decoder DECODER = Base64.getDecoder();
     private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
@@ -22,8 +26,11 @@ public class OrderController {
 
     @GetMapping("/{orderId}")
     public Mono<OrderDetails> getOrderById(@PathVariable long orderId, @RequestHeader("Authorization") String authorization) {
-        final String basic = authorization.substring(authorization.indexOf("Basic "));
-        final String[] split = basic.split(":");
+        final String basic = authorization.substring("Basic".length()).trim();
+        final byte[] credDecoded = DECODER.decode(basic);
+        final String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+
+        final String[] split = credentials.split(":");
         final String username = split[0];
         final String password = split[1];
 
